8372b594f428dc5b033456d7e4a456182dadf8e0
XFanny293 <578337009@qq.com>
2022-09-05 20:32:59 +0800

    operators And Expression

---
+++ operatorsAndExpressions.c
@@ -0,0 +1,13 @@
+//
+// Created on 2022/9/5.
+//
+#include <stdio.h>
+
+int main() {
+    //使被定义的变量的一部分赋初值
+    int a, b, c = 3;
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);//指定a，b，c为整型变量，c的初值为3
+}
+
