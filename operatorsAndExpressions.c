96410b52bbc046401ed149130f567945260790e6
XFanny293 <578337009@qq.com>
2022-09-09 19:06:31 +0800

    upadate

--- operatorsAndExpressions.c
+++ operatorsAndExpressions.c
@@ -21,12 +21,23 @@ int main() {
      y = (int) x % 3;
      printf("x=%f,y=%d\n", x, y);*/
     //第2章课后应用题，题目：输入两个数a、b，交换它们的值，然后输出交换后的结果
-    int a, b, temp;
-    printf("请输入a，b的值,以英文逗号隔开\n");
-    scanf("%d,%d", &a, &b);
-    temp = a;
-    a = b;
-    b = temp;
-    printf("%d,%d", a, b);
+    /* int a, b, temp;
+     printf("请输入a，b的值,以英文逗号隔开\n");
+     scanf("%d,%d", &a, &b);
+     temp = a;
+     a = b;
+     b = temp;
+     printf("%d,%d", a, b);*/
+
+    //用3个getchar函数先从键盘向计算机输入Cxf字符，然后用putchar函数输出
+    char a, b, c;   //定义字符变量a,b,c,d
+    a = getchar();    //从键盘输入一个字符，送给字符变量a、b、c、d
+    b = getchar();
+    c = getchar();
+    putchar(a);     //将变量a的值输出
+    putchar(b);
+    putchar(c);
+    putchar('\n');  //换行
+    return 0;
 }
 
