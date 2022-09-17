afc4d622b4480cd6b6188a1ccdb8003499144cf1
XFanny293 <578337009@qq.com>
2022-09-17 20:09:28 +0800

    learn C_code

--- operatorsAndExpressions.c
+++ operatorsAndExpressions.c
@@ -4,33 +4,33 @@
 #include <stdio.h>
 #include <math.h>   // 程序中要调用求平方根函数sqrt
 
-int main() {
-    /*  //使被定义的变量的一部分赋初值
-      int a, b, c = 3;
-      printf("%d\n", a);
-      printf("%d\n", b);
-      printf("%d\n", c);//指定a，b，c为整型变量，c的初值为3*/
+//int main() {
+/*  //使被定义的变量的一部分赋初值
+  int a, b, c = 3;
+  printf("%d\n", a);
+  printf("%d\n", b);
+  printf("%d\n", c);//指定a，b，c为整型变量，c的初值为3*/
 
-    //2.6运算符和表达式
+//2.6运算符和表达式
 /*    int a = 15, b = 7, d;
     double c = (double) a / b + 0.8;
     d = a / b;
     printf("%f\n", c);
     printf("%d\n", d);*/
-    /* float x = 7.5;
-     int y;
-     y = (int) x % 3;
-     printf("x=%f,y=%d\n", x, y);*/
-    //第2章课后应用题，题目：输入两个数a、b，交换它们的值，然后输出交换后的结果
-    /* int a, b, temp;
-     printf("请输入a，b的值,以英文逗号隔开\n");
-     scanf("%d,%d", &a, &b);
-     temp = a;
-     a = b;
-     b = temp;
-     printf("%d,%d", a, b);*/
+/* float x = 7.5;
+ int y;
+ y = (int) x % 3;
+ printf("x=%f,y=%d\n", x, y);*/
+//第2章课后应用题，题目：输入两个数a、b，交换它们的值，然后输出交换后的结果
+/* int a, b, temp;
+ printf("请输入a，b的值,以英文逗号隔开\n");
+ scanf("%d,%d", &a, &b);
+ temp = a;
+ a = b;
+ b = temp;
+ printf("%d,%d", a, b);*/
 
-    //用3个getchar函数先从键盘向计算机输入Cxf字符，然后用putchar函数输出
+//用3个getchar函数先从键盘向计算机输入Cxf字符，然后用putchar函数输出
 /*    char a, b, c;   //定义字符变量a,b,c,d
     a = getchar();    //从键盘输入一个字符，送给字符变量a、b、c、d
     b = getchar();
@@ -45,6 +45,7 @@ int main() {
     scanf("%f", &b);*/
 //20220910
 //输入三角形的三个边长,求三角形面积
+/*
 
     double a, b, c, s, area;    //定义各变量,均为double型
     printf("请输入三个数字,并用英文逗号分隔\n");
@@ -53,6 +54,9 @@ int main() {
     area = sqrt(s * (s - a) * (s - b) * (s - c)); //计算三角形的面积area
     printf("a=%f\nb=%f\nc=%f\narea=%f\n", a, b, c, area);   //输出结果
     return 0;
-    //record
-}
+*/
+//record
+//    printf("%c\n", getchar() + 32);
+//    return 0;
+//}
 
---
+++ selectStructure.c
@@ -0,0 +1,38 @@
+//
+// Created on 2022/9/15.
+//
+#include <stdio.h>
+
+int main() {
+    //p49真题在线
+    /*   int a, b, t;
+       printf("请输入a,b的值,以英文逗号分隔:");
+       scanf("%d,%d", &a, &b);
+       t = a;
+       a = b;
+       b = t;
+       if (a > b)
+           printf("%d", a);
+       else
+           printf("%d", b);*/
+    //根据函数写出对应的x,y值
+
+    int x, y;
+    printf("请输入一个x的值:\n");
+    scanf("%d", &x);
+    //第一种方法
+//    if (x < 0)
+//        y = -1;
+//    else if (x == 0)
+//        y = 0;
+//    else y = 1;
+
+    //第二种方法
+    if (x >= 0)
+        if (x > 0)y = 1;
+        else y = 0;
+    else
+        y = -1;
+    printf("x=%d,y=%d", x, y);
+    return 0;
+}
