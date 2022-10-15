6c174fb58f97fe23ac3f8ab4fd21fbfdc4bb440c
XFanny293 <578337009@qq.com>
2022-10-15 20:28:22 +0800

    Modified 10.15

--- func.c
+++ func.c
@@ -3,6 +3,7 @@
 //
 #include<stdio.h>
 
+/*
 void Swap(int *pa, int *pb) {
     int z = 0;
     z = *pa;
@@ -17,4 +18,19 @@ int main() {
     Swap(&a, &b);
     printf("交换后：a=%d,b=%d\n", a, b);
     return 0;
+}*/
+int main() {
+    int max(int x, int y);
+    int a, b, c;
+    printf("请输入数字，以逗号隔开:\n");
+    scanf("%d,%d", &a, &b);
+    c = max(a, b);
+    printf("max:%d\n", c);
+    return 0;
+}
+
+int max(int x, int y) {
+    int z;
+    z = x > y ? x : y;
+    return (z);
 }
