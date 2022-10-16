ba6dcfa89289d8ef533ee1e90fa71807ea807f74
XFanny293 <578337009@qq.com>
2022-10-16 14:25:18 +0800

    Modified 10.19

--- func.c
+++ func.c
@@ -19,7 +19,7 @@ int main() {
     printf("交换后：a=%d,b=%d\n", a, b);
     return 0;
 }*/
-int main() {
+/*int main() {
     int max(int x, int y);
     int a, b, c;
     printf("请输入数字，以逗号隔开:\n");
@@ -33,4 +33,78 @@ int max(int x, int y) {
     int z;
     z = x > y ? x : y;
     return (z);
+}*/
+/* //判断是否为素数
+int is_prime(int x) {
+    int j = 0;
+    for (j = 2; j < x; j++) {
+        if (x % j == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    //100~200之间的素数
+    int i = 0;
+    int count = 0;
+    for (i = 100; i <= 200; i++) {
+        //判断i是否为素数
+        if (is_prime(i) == 1) {
+            count++;
+            printf("%d\n", i);
+        }
+    }
+    printf("%d\n", count);
+    return 0;
+}*/
+/* //判断是否为闰年
+int is_leap_year(int year) {
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+int main() {
+    int y = 0;
+    for (y = 1000; y <= 2000; y++) {
+        if (is_leap_year(y) == 1) {
+            printf("%d\n", y);
+        }
+    }
+}*/
+/*
+int binary_search(int a[], int k, int s) {
+    int l = 0, r = s - 1;
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (a[mid] > k) {
+            r = mid - 1;
+        } else if (a[mid] < k) {
+            l = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;//找不到的情况
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int key = 7;
+    int sz = sizeof(arr) / sizeof(arr[0]);
+    //找到就返回找到的位置的下标
+    //找不到就返回-1
+    int ret = binary_search(arr, key, sz);
+    if (-1 != ret) {
+        printf("找不到\n");
+    } else {
+        printf("找到了，下标是:%d\n", ret);
+    }
+    return 0;
+}*/
+int main() {
+
 }
