be19a33ba508a121a15a0f16297914a77776e093
XFanny293 <578337009@qq.com>
2022-11-20 11:26:34 +0800

    Modified Nov 20

--- Learn1/pointerAdvanced.c
+++ Learn1/pointerAdvanced.c
@@ -45,17 +45,128 @@ int main() {
     return 0;
 }*/
 
-int main() {
-    int arr[10] = {0};
+/*int main() {
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int (*pa)[10] = &arr;
+    int i = 0;
+    for (i = 0; i < 10; i++) {
+        printf("%d ", *((*pa) + i));//1 2 3 4 5 6 7 8 9 10
+    }
 
+}*/
+/*
+void print1(int arr[3][5], int r, int c) {
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
 }
 
+//指针写法
+//p是一个数组指针，指向某一行
+void print2(int (*p)[5], int r, int c) {
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < r; i++)
+        for (i = 0; i < r; i++) {
+            for (j = 0; j < c; j++) {
+                printf("%d ", *(*(p + i) + j));
+            }
+            printf("\n");
+        }
+}
 
+int main() {
+    int arr[3][5] = {{1, 2, 3, 4, 5},
+                     {2, 3, 4, 5, 6},
+                     {3, 4, 5, 6, 7}};
+//    print1(arr, 3, 5);
+    print2(arr, 3, 5);//arr数组名，表示数组首元素的地址
+    //二维数组的首元素是：第一行
+    return 0;
+}
+*/
+//一维数组传参
+/*void test(int arr[]) {} //ok
 
+void test(int arr[10]) {} //ok
 
+void test(int *arr) {} //ok
 
+void test2(int *arr[20]) {}//ok
 
+void test2(int **arr) {}//ok
 
+int main() {
+    int arr[10] = {0};
+    int *arr2[20] = {0};
+    test(arr);
+    test2(arr2);
+}*/
+
+//二维数组传参
+/*void test(int arr[3][5]) {} //ok
+void test(int arr[][]) {}   //NOT
+void test(int arr[][5]) {}  //ok
+//总结:二维数组传参，函数形参的设计只能省略第一个[]的数字。
+//因为对一个二维数组，可以不知道有多少行，但是必须知道一行多少元素
+//这样才方便运算
+void test(int *arr) {}
+
+void test(int *arr[5]) {}
+
+void test(int (*arr)[5]) {}
+
+void test(int **arr) {}
+
+int main() {
+    int arr[3][5] = {0};
+    test(arr);
+}*/
+//一级指针传参
+/*void print(int *p, int sz) {
+    int i = 0;
+    for (i = 0; i < sz; i++) {
+        printf("%d\n", *(p + i));
+    }
+}
+
+int main() {
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int *p = arr;
+    int sz = sizeof(arr) / sizeof(arr[0]);
+    //一级指针p，传给函数
+    return 0;
+}*/
+//二级指针传参
+/*void test(int **ptr) {
+    printf("num=%d\n", **ptr);
+}
+
+int main() {
+    int n = 10;
+    int *p = &n;
+    int **p = &p;
+    test(pp);
+    test(&p);
+    return 0;
+}*/
+void test(char *pp) {}
+
+int main() {
+    char c = 'x';
+    char *pc = &c;
+    char **ppc = &pc;
+    char *arr[10];
+    test(&pc);
+    test(ppc);
+    test(arr);
+    return 0;
+}
 
 
 
